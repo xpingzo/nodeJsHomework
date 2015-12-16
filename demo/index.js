7eb5b5ce15ccd124bb44789bd4514f37c810e458
xpingzo <xpingzo@163.com>
2015-12-16 16:55:22 +0800

    优化用async模块写的结构

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,8 @@
 var server = require("../lib/index");
 var port = 9999;
+var config = {
+    fileMatch: /^(gif|png|jpg|js|css)$/ig,
+    maxAge: 60*60*24*365
+};
 
-var server1 = server.createServer(port);
+var server1 = server.createServer(port, config);
